eff42075a6a6266f2e47a1fb8f2a6b03d2239785
YuanYi <yuanyigre@126.com>
2017-02-24 11:55:52 +0800

    Add files via upload

---
+++ statis_yy3.py
@@ -0,0 +1,44 @@
+#--coding:utf-8--
+the_file = open('happiness_seg.txt','r') 
+data = {}
+
+for line in the_file:
+    line = line.strip()
+    if len(line) > 0:
+        print line
+        import re
+        words = re.split(r'[\s]',line)
+        if len(words) > 40:
+            print '-------------',words[9],words[10]
+
+       
+        if len(words) >= 2:
+            length = len(words)
+            count = 0
+            while count < length-1:            
+                item1 = words[count]
+                item2 = words[count+1]
+                print '**********',item1,item2
+                if item2 not in ['。','，','：','；','？','?','―']:
+                    item = item1 + ' ' + item2
+                    print item
+                    count = count + 1
+                    if item in data:
+                        data[item] += 1
+                    else:
+                        data[item] = 1
+                else:
+                    count = count + 2 
+        
+the_file.close()
+
+def sort_by_value(d):      # to sort out the 10 most frequent words
+    items = d.items()
+    rev_items = [[v[1],v[0]]for v in items]
+    rev_items.sort(reverse = True)
+    step = 0
+    while step < 10:
+        print rev_items[step][1], rev_items[step][0]
+        step += 1
+
+sort_by_value(data)
